08022c4406c8222d02b217ba4a402dc789327fed
Nick <119859245+nicklySS@users.noreply.github.com>
2024-01-19 11:54:12 +0300

    Edit script.js
    Make addBookToLibrary and printBooks functions

--- script.js
+++ script.js
@@ -14,40 +14,63 @@ function Book(name, author, pageNum, read) {
 }
 
 function addBookToLibrary() {
+    const form = document.querySelector("#modal-form");
+    const inputs = form.querySelectorAll("input");
 
+    const newBook = {
+        name: inputs[0].value,
+        author: inputs[1].value,
+        pageNum: Number(inputs[2].value),
+        read: inputs[3].checked,
+    };
+
+    myLibrary.push(
+        new Book(newBook.name, newBook.author, newBook.pageNum, newBook.read)
+    );
+
+    for (const input of inputs) {
+        input.value = "";
+    }
+
+    printBooks();
+
+    // form.submit();
 }
 
-
-
 const hobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
-const witcher = new Book("The Witcher", "A.Sapkovsky",421,true);
-const warcraft = new Book("Warcraft 3","Someone",581,true);
+const witcher = new Book("The Witcher", "A.Sapkovsky", 421, true);
+const warcraft = new Book("Warcraft 3", "Someone", 581, true);
 
 myLibrary.push(hobbit);
 myLibrary.push(witcher);
 myLibrary.push(warcraft);
 
-myLibrary.forEach((book) => {
-    const card = document.createElement("div");
-    card.classList.add("card");
-
-    card.innerHTML = `
-        <h1 class="name">${book.name}</h1>
-        <h2 class="author">${book.author}</h2>
-        <h2 class="page-num">${book.pageNum}</h2>
-        <div class="read">
-            <h2 class="read-txt">Read?</h2>
-            <div class="read-tumbler"></div>
-        </div>
-    `;
-
-    document.getElementById("cards").appendChild(card);
-});
+function printBooks() {
+    document.querySelector("#cards").innerHTML = "";
+    myLibrary.forEach((book) => {
+        const card = document.createElement("div");
+        card.classList.add("card");
+    
+        card.innerHTML = `
+            <h1 class="name">${book.name}</h1>
+            <h2 class="author">${book.author}</h2>
+            <h2 class="page-num">${book.pageNum}</h2>
+            <div class="read">
+                <h2 class="read-txt">Read?</h2>
+                <div class="read-tumbler"></div>
+            </div>
+        `;
+    
+        document.getElementById("cards").appendChild(card);
+    });
+}
 
 function showAddWindow() {
-   document.querySelector(".modal").style.cssText = "left: 0; display: block;";
+    document.querySelector(".modal").style.cssText = "left: 0;";
 }
 function closeAddWindow() {
     document.querySelector(".modal").style.cssText = "left: -100%;";
-    
 }
+
+
+printBooks();
