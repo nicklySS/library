47202aef545bb54140dc04190367a6da64ab5e3a
Nick <119859245+nicklySS@users.noreply.github.com>
2024-01-20 13:05:00 +0300

    Edit script.js
    Added functionality for toggle switch and remove button

--- script.js
+++ script.js
@@ -49,18 +49,55 @@ function printBooks() {
     document.querySelector("#cards").innerHTML = "";
     myLibrary.forEach((book) => {
         const card = document.createElement("div");
+        const readStatus = book.read ? "Read" : "Not read yet";
+        const bookIndex = myLibrary.indexOf(book);
+
         card.classList.add("card");
-    
+
         card.innerHTML = `
             <h1 class="name">${book.name}</h1>
             <h2 class="author">${book.author}</h2>
             <h2 class="page-num">${book.pageNum}</h2>
             <div class="read">
-                <h2 class="read-txt">Read?</h2>
-                <div class="read-tumbler"></div>
+                <h2 class="read-txt">${readStatus}</h2>
+                <div class="read-switch"><div class="read-switch-round"></div></div>
             </div>
+            <button class="remove-btn">Remove</button>
         `;
-    
+
+        const readStatusTxt = card.querySelector(".read-txt");
+        const switchRead = card.querySelector(".read-switch");
+        const switchRound = card.querySelector(".read-switch-round");
+        const removeBtn = card.querySelector(".remove-btn");
+
+        switchRead.style.cssText = `background-color: ${
+            book.read ? "blue" : "ghostwhite"
+        }`;
+        switchRound.style.cssText = `background-color: ${
+            book.read ? "white" : "lightgray"
+        }`;
+        switchRead.style.justifyContent = book.read ? "right" : "left";
+
+        switchRead.addEventListener("click", () => {
+            myLibrary[bookIndex].read = !myLibrary[bookIndex].read;
+
+            readStatusTxt.textContent = book.read ? "Read" : "Not read yet";
+
+            switchRead.style.cssText = `background-color: ${
+                book.read ? "blue" : "ghostwhite"
+            }`;
+            switchRound.style.cssText = `background-color: ${
+                book.read ? "white" : "lightgray"
+            }`;
+
+            switchRead.style.justifyContent = book.read ? "right" : "left";
+        });
+
+        removeBtn.addEventListener("click", (event) => {
+            myLibrary.splice(bookIndex, 1);
+            printBooks();
+        });
+
         document.getElementById("cards").appendChild(card);
     });
 }
@@ -71,6 +108,5 @@ function showAddWindow() {
 function closeAddWindow() {
     document.querySelector(".modal").style.cssText = "left: -100%;";
 }
-
 
 printBooks();
